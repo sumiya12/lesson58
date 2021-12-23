23d88bdfff267046ecd7fe4386e821a84720b695
sumiya12 <sumiya.udval@gmail.com>
2021-12-23 21:50:36 +0800

    IIFE imedietily invoked function expression

---
+++ app.js
@@ -0,0 +1,19 @@
+// imedietily invoked function expression it means IIFE
+// Function statement
+// function hi() {
+//   console.log("hi");
+// }
+// hi();
+
+// aaa();
+// // Function expression
+// var aaa = function () {
+//   console.log("hello");
+// };
+// aaa();
+
+// nergvi function duudaj nen darui duudah, huurah arga
+(function () {
+  console.log("hihihi");
+})();
+// private , data encapsulation ,data protect
